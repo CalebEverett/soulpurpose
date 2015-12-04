3df8ad6bf6648495d0d54fd3fb5b1c0260e2b283
Caleb Everett <caleb@careercalifornia.edu>
2015-12-04 02:20:18 -0800

    Added responsive video background to home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {overlayStyle: 'overlay'};
+  }
+
   render() {
     const styles = require('./Home.scss');
+    const overlayStyle = this.state.overlayStyle;
 
     const handleOnCanPlay = (event) => {
+      this.setState({overlayStyle: 'overlayVideo'});
+      console.log(this.state.overlayState);
       setTimeout(function playVid() {
         event.target.play();
       }, 3000);
@@ -13,16 +21,16 @@ export default class Home extends Component {
     return (
       <div className={styles.home}>
         <div className={styles.mastheadContainer}>
-          <img className={styles.posterImage} src={require('./niko.jpg')} />
+          <div className={styles.posterImage} />
           <video className={styles.mastheadVideo} src={require('./nikovideo.mp4')} type="video/mp4" preload="true" onCanPlay={handleOnCanPlay} loop="loop" />
-          <div className={styles.overlay} />
+          <div className={styles[overlayStyle]} />
           <div className={styles.mastheadText}>
             <h1>SOUL PURPOSE</h1>
             <h3>Let’s see our greatness, taste our desire and feel our passion!</h3>
           </div>
         </div>
         <section className={styles.inspirationSection}>
-          <h2>Let’s believe in something big and reach beyond our grasp. Let’s fall and get back up. Let’s SHINE! Let’s feel good and happy and alive. Let’s set this world on FIRE!</h2>
+          <h2>Let’s believe in something big and reach beyond our grasp. Let’s fall and get back up. Let’s <strong>SHINE!</strong> Let’s feel good and happy and alive. Let’s set this world on <strong>FIRE!</strong></h2>
         </section>
         <section className={styles.featureSection}>
           <div className={styles.featureBlog}>
